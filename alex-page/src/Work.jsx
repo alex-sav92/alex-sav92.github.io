ca4feaa122541b7bf0019f4fc9eea9d76c8657d7
alex-sav92 <alexandrasavescu92@gmail.com>
2023-02-10 18:30:04 +0200

    update

--- alex-page/src/Work.jsx
+++ alex-page/src/Work.jsx
@@ -1,49 +1,103 @@
 import React from "react";
-import {useRef, useEffect, useState} from 'react';
 
 const Work = () => {
-    const [toggle1, setToggle1] = useState(false)
-    const [toggle2, setToggle2] = useState(false)
-    const [toggle3, setToggle3] = useState(false)
-    const [toggle4, setToggle4] = useState(false)
-    
     return (
-        <div className="work">
-            <div className='card'>
-                <h2>Genesis @ Maxcode</h2>
-                <p className='tech'>
-                   C# ASP.NET Web API AWS Lambda AWS API Gateway AWS Cognito MongoDB SQL Server MongoDB's Device Sync
-                </p>
-                <p className={toggle1? 'more active' : 'more'} onClick={() => setToggle1(!toggle1)}>🡇</p>
+            <div className="projects">
+                <a className="card" href="#!">
+                    <div className="front">
+                        <p>Genesis @ Maxcode</p>
+                    </div>
+                    <div className="back">
+                        <div>
+                            <h3>Genesis @ Maxcode</h3>
+                            <p>Revolutionizing the healthcare industry with software that is also available in offline scenarios. Built using NoSQL Databases, opearting on mobile devices, bringing high availability and great performance by leveraging cloud's power.</p>
+                            <p>Technologies:</p>
+                            <div className="tech">
+                                <span>C#</span> 
+                                <span>ASP.NET Web API</span> 
+                                <span>AWS Lambda</span> 
+                                <span>AWS API Gateway</span> 
+                                <span>AWS Cognito</span> 
+                                <span>MongoDB</span> 
+                                <span>SQL Server</span> 
+                                <span>MongoDB's Device Sync</span>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <a className="card" href="#!">
+                    <div className="front">
+                        <p>Pharmaceutical @ Softvision</p>
+                    </div>
+                    <div className="back">
+                        <div>
+                            <h3>Pharmaceutical @ Softvision</h3>
+                            <p>Adapting a Pharmaceutical solution for the Canada's government-required Accessibility standards. Includes working with industry-wide accessibility levels such as: A/AA/AAA, researching W3 documentation.</p>
+                            <p>Technologies:</p>
+                            <div className="tech">
+                                <span>C#</span> 
+                                <span>ASP.NET</span> 
+                                <span>Angular</span> 
+                                <span>Web design</span> 
+                                <span>HTML</span> 
+                                <span>ARIA-attributes </span> 
+                                <span>CSS</span> 
+                                <span>JavaScript</span>
+                                <span>screen reader</span>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+                <a className="card" href="#!">
+                    <div className="front">
+                    <p>Liveforever club</p>
+                    </div>
+                    <div className="back">
+                        <div>
+                            <h3>Liveforever club</h3>
+                            <p>My long-term freelance project, helping my client achieve his goals in creating a life-extension community. I helped build, extend and maintain the portal for publishing blog articles, the CMS associated to it, and also I helped in implementing the design for the foundational book of this club - LiveForever Manual.</p>
+                            <p>Technologies:</p>
+                            <div className="tech">
+                                <span>C#</span> 
+                                <span>ASP.NET MVC</span> 
+                                <span>SQL Server</span> 
+                                <span>HTML</span> 
+                                <span>CSS</span> 
+                                <span>javascript</span> 
+                                <span>jQuery</span> 
+                                <span>iText</span>
+                            </div>
+                            <a className="button" href="https://liveforever.club" target="blank">visit website</a>
+                            <a className="button" href="https://www.amazon.co.uk/dp/B07LB17TNW/ref=as_li_ss_tl?ie=UTF8&linkCode=gs2&tag=s08a-21" target="blank">view book</a>
+                        </div>
+                    </div>
+                </a>
+                <a className="card" href="#!">
+                    <div className="front">
+                    <p>Hallmark NL</p>
+                    </div>
+                    <div className="back">
+                        <div>
+                            <h3>Hallmark NL</h3>
+                            <p>A long term collaboration in which I helped extend the client-facing web apps for creating, customizing and sending greeting cards and gifts. Helped with implementation of promotional features, contributed in feature for "suggested gifts", extending and maintaining the CMS and the Admininstration Panels for the Customer Service department.</p>
+                            <p>Technologies:</p>
+                            <div className="tech">
+                                <span>C#</span> 
+                                <span>ASP.NET MVC</span> 
+                                <span>ASP.NET Web Forms</span> 
+                                <span>HTML</span> 
+                                <span>CSS</span> 
+                                <span>javascript</span> 
+                                <span>jQuery</span> 
+                                <span>SQL Server</span>
+                                <span>LLbLGen</span>
+                                <span>Azure</span>
+                            </div>
+                            <a className="button" href="https://www.hallmark.nl" target="blank">visist website</a>
+                        </div>
+                    </div>
+                </a>
             </div>
-            <div className='card'>
-                <h2>Pharmaceutical @ Softvision</h2>
-                <p className='tech'>
-                   ASP.NET Angular Web design Accessibility HTML Aria-attributes CSS JavaScript Screen reader
-                </p>
-                <p  className={toggle2? 'more active' : 'more'} onClick={() => setToggle2(!toggle2)}>🡇</p>
-                {toggle2 && (
-                    <p>
-                       Adapting a Pharmaceutical solution for the Canada's government-required Accessibility standards. Includes working with industry-wide accessibility levels such as: A/AA/AAA, researching W3 documentation. 
-                    </p>
-                )}
-            </div>
-            <div className='card'>
-                <h2>Liveforever club </h2>
-                <p className='tech'>
-                   ASP.NET MVC SQL Server HTML CSS JavaScript jQuery
-                </p>
-                <p className="more" onClick={() => setToggle3(!toggle3)}>🡇</p>
-            </div>
-            <div className='card'>
-                <h2>Hallmark</h2>
-                <p className='tech'>
-                   ASP.NET MVC ASP.NET Web Forms HTML CSS JavaScript jQuery SQL Server LLbLGen
-                </p>
-                <p className="more" onClick={() => setToggle4(!toggle4)}>🡇</p>
-
-            </div>
-        </div>
     );
 };
 
