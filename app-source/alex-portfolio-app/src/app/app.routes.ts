fa4c25f04bbe8094e8257916a1198ea0e145c61d
alex-sav92 <alexandrasavescu92@gmail.com>
2026-03-30 21:49:27 +0300

    Update main page - links to apps

--- app-source/alex-portfolio-app/src/app/app.routes.ts
+++ app-source/alex-portfolio-app/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { BlogComponent } from './blog/blog.component';
 import { AboutComponent } from './about/about.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 export const routes: Routes = [
-    { path: '', component: BlogComponent },
+    { path: '', component: HomeComponent },
     { path: 'blog/:seoTitle', component: BlogComponent },
     { path: 'blog', component: BlogComponent },
     { path: 'about', component: AboutComponent },
